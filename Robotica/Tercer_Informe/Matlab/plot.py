b9ce53138e966cead4fd7122785799ea8078f747
hvzzzz <hananquispec@outlook.com>
2020-06-01 03:18:27 -0500

    this_time

--- Robotica/Tercer_Informe/Matlab/plot.py
+++ Robotica/Tercer_Informe/Matlab/plot.py
@@ -1,11 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-t = np.arange(-20.0, 20.0, 0.001)
-s = np.tanh(t)/(1+np.tanh(t)*np.tanh(t))
+def sech(t):
+    return(1/np.cosh(t))
+t = np.arange(-20.0, 20.0, 0.001)#[np.newaxis] este es para poder sacarle la transpuesta a 
+#s = np.tanh(t)/((1+np.power(np.tanh(t),2) )*np.power(np.cosh(t),2))
+#s=-np.log(np.power(np.tanh(t),2)+1)
+s=-(2*np.power(np.tanh(t),2)*np.power(sech(t),4))/np.power(1+np.power(np.tanh(t),2),2)+np.power(sech(t),4)/(1+np.power(np.tanh(t),2))-2*np.power(np.tanh(t),2)*np.power(sech(t),2)/(1+np.power(np.tanh(t),2))
+#ss=s.T
+#print(s)
+#print(ss)
+#euclidean=np.sqrt(s.dot(ss))
+#print(euclidean)
+#s=np.sqrt(np.log(np.power(np.tanh(t),2)+1))
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 plt.plot(t, s)
 plt.grid(True)
+plt.xlabel(r'$\tilde{q}$',fontsize=16)
+plt.ylabel(r'$\frac{1}{cosh^2(\tilde{q})}\frac{tanh(\tilde{q})}{1+tanh^2(\tilde{q})}$',fontsize=16)
 plt.savefig('done.png')
-s = np.power(np.tanh(t),1)/(np.power(np.sinh(t),1)*np.cosh(t))
-plt.plot(t, s)
-plt.grid(True)
-plt.savefig('done1.png')
+#s = np.power(np.tanh(t),1)/(np.power(np.sinh(t),1)*np.cosh(t))
+#lt.plot(t, s)
+#plt.grid(True)
+#plt.savefig('done1.png')
